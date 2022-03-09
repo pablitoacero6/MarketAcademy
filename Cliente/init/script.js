8f34caa3099b024e721e30c34ce2d04544946d3b
pablitoacero6 <51001188+pablitoacero6@users.noreply.github.com>
2022-03-09 16:33:40 -0500

    create register teacher

--- Cliente/init/script.js
+++ Cliente/init/script.js
@@ -1,14 +1,28 @@
-var register = document.getElementById("register"),
+var registerStudent = document.getElementById("registerStudent"),
+    registerTeacher = document.getElementById("registerTeacher"),
     overlay1 =document.getElementById("overlay1"),
+    overlay2 =document.getElementById("overlay2"),
     popup1 = document.getElementById("popup1"),
-    cerrarPopup1 = document.getElementById("cerrarPopup1");
+    popup2 = document.getElementById("popup2"),
+    cerrarPopup1 = document.getElementById("cerrarPopup1"),
+    cerrarPopup2 = document.getElementById("cerrarPopup2");
 
-    register.addEventListener('click', function() {
+    registerStudent.addEventListener('click', function() {
         overlay1.classList.add('active');
         popup1.classList.add('active');
+    });
+
+    registerTeacher.addEventListener('click', function(){
+        overlay2.classList.add('active');
+        popup2.classList.add('active');
     });
 
     cerrarPopup1.addEventListener('click', function() {
         overlay1.classList.remove('active');
         popup1.classList.remove('active');
+    });
+
+    cerrarPopup2.addEventListener('click', function() {
+        overlay2.classList.remove('active');
+        popup2.classList.remove('active');
     });
