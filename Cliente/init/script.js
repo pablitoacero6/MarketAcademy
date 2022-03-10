bd3f35e270c2eb87a6313f8f23d55b592df4459b
pablitoacero6 <51001188+pablitoacero6@users.noreply.github.com>
2022-03-10 17:51:05 -0500

    add search

--- Cliente/init/script.js
+++ Cliente/init/script.js
@@ -3,13 +3,15 @@ var registerStudent = document.getElementById("registerStudent"),
     popup1 = document.getElementById("popup1"),
     cerrarPopup1 = document.getElementById("cerrarPopup1");
 
+    /* POPUP */
+
     registerStudent.addEventListener('click', function() {
         overlay1.classList.add('active');
         popup1.classList.add('active');
     });
 
-
     cerrarPopup1.addEventListener('click', function() {
         overlay1.classList.remove('active');
         popup1.classList.remove('active');
     });
+
